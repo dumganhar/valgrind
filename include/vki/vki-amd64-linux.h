dumganhar/valgrind

commit 60457093d30b23ac2531682205ab0bd9a5aae2ed

Make client sys_shmat work properly on arm-linux by taking into account rounding requirements to SHMLBA.  Modified version of a patch by Kirill Batuzov, batuzovk@ispras.ru.  This fixes the main bug in #222545.  Temporarily breaks the build on all other platforms though.
git-svn-id: svn://svn.valgrind.org/valgrind/trunk@11399 a5019735-40e9-0310-863c-91ae7b9d1cf9

include/vki/vki-arm-linux.h
@@ -731,6 +731,8 @@ struct vki_ipc_kludge {
 #define VKI_SHMGET		23
 #define VKI_SHMCTL		24
 
+#define VKI_SHMLBA  (4 * VKI_PAGE_SIZE)
+
 
 //----------------------------------------------------------------------
 // From linux-2.6.8.1/include/asm-i386/shmbuf.h

coregrind/m_syswrap/syswrap-arm-linux.c
@@ -831,6 +831,12 @@ PRE(wrap_sys_shmat)
    PRINT("wrap_sys_shmat ( %ld, %#lx, %ld )",ARG1,ARG2,ARG3);
    PRE_REG_READ3(long, "shmat",
                  int, shmid, const void *, shmaddr, int, shmflg);
+   /* Round the attach address down to an VKI_SHMLBA boundary if the
+      client requested rounding.  See #222545.  This is necessary only
+      on arm-linux because VKI_SHMLBA is 4 * VKI_PAGE size; on all
+      other linux targets it is the same as the page size. */
+   if (ARG3 & VKI_SHM_RND)
+      ARG2 = VG_ROUNDDN(ARG2, VKI_SHMLBA);
    arg2tmp = ML_(generic_PRE_sys_shmat)(tid, ARG1,ARG2,ARG3);
    if (arg2tmp == 0)
       SET_STATUS_Failure( VKI_EINVAL );

coregrind/m_syswrap/syswrap-generic.c
@@ -1741,9 +1741,26 @@ ML_(generic_PRE_sys_shmat) ( ThreadId tid,
    UWord tmp;
    Bool  ok;
    if (arg1 == 0) {
+      /* arm-linux only: work around the fact that
+         VG_(am_get_advisory_client_simple) produces something that is
+         VKI_PAGE_SIZE aligned, whereas what we want is something
+         VKI_SHMLBA aligned, and VKI_SHMLBA >= VKI_PAGE_SIZE.  Hence
+         increase the request size by VKI_SHMLBA - VKI_PAGE_SIZE and
+         then round the result up to the next VKI_SHMLBA boundary.
+         See bug 222545 comment 15.  So far, arm-linux is the only
+         platform where this is known to be necessary. */
+      vg_assert(VKI_SHMLBA >= VKI_PAGE_SIZE);
+      if (VKI_SHMLBA > VKI_PAGE_SIZE) {
+         segmentSize += VKI_SHMLBA - VKI_PAGE_SIZE;
+      }
       tmp = VG_(am_get_advisory_client_simple)(0, segmentSize, &ok);
-      if (ok)
-         arg1 = tmp;
+      if (ok) {
+         if (VKI_SHMLBA > VKI_PAGE_SIZE) {
+            arg1 = VG_ROUNDUP(tmp, VKI_SHMLBA);
+         } else {
+            arg1 = tmp;
+         }
+      }
    }
    else if (!ML_(valid_client_addr)(arg1, segmentSize, tid, "shmat"))
       arg1 = 0;

include/vki/vki-linux.h
@@ -1451,6 +1451,7 @@ struct vki_shmid_ds {
 };
 
 #define VKI_SHM_RDONLY  010000  /* read-only access */
+#define VKI_SHM_RND     020000  /* round attach address to SHMLBA boundary */
 
 #define VKI_SHM_STAT 	13
 #define VKI_SHM_INFO 	14

commit 1fe372ce861a7b4b567b1e1d7cc84cd8375023cd

Define VKI_SHMLBA for all supported Linux targets, thereby unbreaking the breakage created by r11399.  Part of #222545.

include/vki/vki-amd64-linux.h
@@ -63,6 +63,12 @@ typedef unsigned int vki_u32;
 #define VKI_MAX_PAGE_SIZE	VKI_PAGE_SIZE
 
 //----------------------------------------------------------------------
+// From linux-2.6.35.4/arch/x86/include/asm/shmparam.h
+//----------------------------------------------------------------------
+
+#define VKI_SHMLBA  VKI_PAGE_SIZE
+
+//----------------------------------------------------------------------
 // From linux-2.6.9/include/asm-x86_64/signal.h
 //----------------------------------------------------------------------
 

include/vki/vki-arm-linux.h
@@ -64,6 +64,12 @@ typedef unsigned int vki_u32;
 #define VKI_MAX_PAGE_SIZE	VKI_PAGE_SIZE
 
 //----------------------------------------------------------------------
+// From linux-2.6.35.4/arch/arm/include/asm/shmparam.h
+//----------------------------------------------------------------------
+
+#define VKI_SHMLBA  (4 * VKI_PAGE_SIZE)
+
+//----------------------------------------------------------------------
 // From linux-2.6.8.1/include/asm-i386/signal.h
 //----------------------------------------------------------------------
 
@@ -730,9 +736,6 @@ struct vki_ipc_kludge {
 #define VKI_SHMDT		22
 #define VKI_SHMGET		23
 #define VKI_SHMCTL		24
-
-#define VKI_SHMLBA  (4 * VKI_PAGE_SIZE)
-
 
 //----------------------------------------------------------------------
 // From linux-2.6.8.1/include/asm-i386/shmbuf.h

include/vki/vki-ppc32-linux.h
@@ -69,6 +69,12 @@ extern unsigned long VKI_PAGE_SIZE;
 #define VKI_MAX_PAGE_SIZE	(1UL << VKI_MAX_PAGE_SHIFT)
 
 //----------------------------------------------------------------------
+// From linux-2.6.35.4/arch/powerpc/include/asm/shmparam.h
+//----------------------------------------------------------------------
+
+#define VKI_SHMLBA  VKI_PAGE_SIZE
+
+//----------------------------------------------------------------------
 // From linux-2.6.9/include/asm-ppc/signal.h
 //----------------------------------------------------------------------
 

include/vki/vki-ppc64-linux.h
@@ -70,6 +70,12 @@ extern unsigned long VKI_PAGE_SIZE;
 #define VKI_MAX_PAGE_SIZE	(1UL << VKI_MAX_PAGE_SHIFT)
 
 //----------------------------------------------------------------------
+// From linux-2.6.35.4/arch/powerpc/include/asm/shmparam.h
+//----------------------------------------------------------------------
+
+#define VKI_SHMLBA  VKI_PAGE_SIZE
+
+//----------------------------------------------------------------------
 // From linux-2.6.13/include/asm-ppc64/signal.h
 //----------------------------------------------------------------------
 

include/vki/vki-x86-linux.h
@@ -64,6 +64,12 @@ typedef unsigned int vki_u32;
 #define VKI_MAX_PAGE_SIZE	VKI_PAGE_SIZE
 
 //----------------------------------------------------------------------
+// From linux-2.6.35.4/arch/x86/include/asm/shmparam.h
+//----------------------------------------------------------------------
+
+#define VKI_SHMLBA  VKI_PAGE_SIZE
+
+//----------------------------------------------------------------------
 // From linux-2.6.8.1/include/asm-i386/signal.h
 //----------------------------------------------------------------------
 