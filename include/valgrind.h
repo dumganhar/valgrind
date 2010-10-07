dumganhar/valgrind

commit 602278444fa6b4135a44d19ae833ed42a9898e48

Tidy up ifdeffery a bit.  (non-functional change)
git-svn-id: svn://svn.valgrind.org/valgrind/trunk@11408 a5019735-40e9-0310-863c-91ae7b9d1cf9

include/valgrind.h
@@ -204,7 +204,8 @@
 
 /* ------------------------- x86-{linux,darwin} ---------------- */
 
-#if defined(PLAT_x86_linux)  ||  defined(PLAT_x86_darwin)  ||  defined(PLAT_x86_win32) && defined(__GNUC__)
+#if defined(PLAT_x86_linux)  ||  defined(PLAT_x86_darwin)  
+    ||  (defined(PLAT_x86_win32) && defined(__GNUC__))
 
 typedef
    struct { 
@@ -254,7 +255,7 @@ typedef
                      __SPECIAL_INSTRUCTION_PREAMBLE               \
                      /* call-noredir *%EAX */                     \
                      "xchgl %%edx,%%edx\n\t"
-#endif /* PLAT_x86_linux || PLAT_x86_darwin || PLAT_x86_win32 && __GNUC__ */
+#endif /* PLAT_x86_linux || PLAT_x86_darwin || (PLAT_x86_win32 && __GNUC__) */
 
 /* ------------------------- x86-Win32 ------------------------- */
 

commit 2203d91065a8a6853e46c642e7821f4dbb94a350

Fix build failure I introduced in r11408, duh.

include/valgrind.h
@@ -204,7 +204,7 @@
 
 /* ------------------------- x86-{linux,darwin} ---------------- */
 
-#if defined(PLAT_x86_linux)  ||  defined(PLAT_x86_darwin)  
+#if defined(PLAT_x86_linux)  ||  defined(PLAT_x86_darwin)  \
     ||  (defined(PLAT_x86_win32) && defined(__GNUC__))
 
 typedef