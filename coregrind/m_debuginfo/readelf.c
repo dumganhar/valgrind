dumganhar/valgrind

commit 0e1e39959ee3446bb1ce747779acb895d76c0231

Valgrind is able to read compressed debuginfo sections in two formats:   - zlib ELF gABI format with SHF_COMPRESSED flag (gcc option -gz=zlib)   - zlib GNU format with .zdebug sections (gcc option -gz=zlib-gnu)
Patch by: Aleksandar Rikalo <aleksandar.rikalo@imgtec.com>
Fixes BZ#303877



git-svn-id: svn://svn.valgrind.org/valgrind/trunk@15868 a5019735-40e9-0310-863c-91ae7b9d1cf9

coregrind/m_debuginfo/readelf.c
@@ -59,6 +59,33 @@
 #endif
 /* --- !!! --- EXTERNAL HEADERS end --- !!! --- */
 
+#if !defined(HAVE_ELF32_CHDR)
+   typedef struct {
+      Elf32_Word  ch_type;
+      Elf32_Word  ch_size;
+      Elf32_Word  ch_addralign;
+   } Elf32_Chdr;
+#endif
+
+#if !defined(HAVE_ELF64_CHDR)
+   typedef struct {
+      Elf64_Word  ch_type;
+      Elf64_Word  ch_reserved;
+      Elf64_Xword ch_size;
+      Elf64_Xword ch_addralign;
+   } Elf64_Chdr;
+#endif
+
+#if !defined(SHF_COMPRESSED)
+   #define SHF_COMPRESSED (1 << 11)
+#endif
+
+#if !defined(ELFCOMPRESS_ZLIB)
+   #define ELFCOMPRESS_ZLIB 1
+#endif
+
+#define SIZE_OF_ZLIB_HEADER 12
+
 /*------------------------------------------------------------*/
 /*--- 32/64-bit parameterisation                           ---*/
 /*------------------------------------------------------------*/
@@ -80,6 +107,7 @@
 #  define  ElfXX_Dyn      Elf32_Dyn
 #  define  ELFXX_ST_BIND  ELF32_ST_BIND
 #  define  ELFXX_ST_TYPE  ELF32_ST_TYPE
+#  define  ElfXX_Chdr     Elf32_Chdr
 
 #elif VG_WORDSIZE == 8
 #  define  ElfXX_Ehdr     Elf64_Ehdr
@@ -93,6 +121,7 @@
 #  define  ElfXX_Dyn      Elf64_Dyn
 #  define  ELFXX_ST_BIND  ELF64_ST_BIND
 #  define  ELFXX_ST_TYPE  ELF64_ST_TYPE
+#  define  ElfXX_Chdr     Elf64_Chdr
 
 #else
 # error "VG_WORDSIZE should be 4 or 8"
@@ -1436,6 +1465,50 @@ Word file_offset_from_svma ( /*OUT*/Bool* ok,
    return 0;
 }
 
+/* Check if section is compressed and modify DiSlice if it is.
+   Returns False in case of unsupported compression type.
+*/
+static Bool check_compression(ElfXX_Shdr* h, DiSlice* s) {
+   if (h->sh_flags & SHF_COMPRESSED) {
+      ElfXX_Chdr chdr;
+      ML_(img_get)(&chdr, s->img, s->ioff, sizeof(ElfXX_Chdr));
+      if (chdr.ch_type != ELFCOMPRESS_ZLIB)
+         return False;
+      s->ioff = ML_(img_mark_compressed_part)(s->img,
+                                              s->ioff + sizeof(ElfXX_Chdr),
+                                              s->szB - sizeof(ElfXX_Chdr),
+                                              (SizeT)chdr.ch_size);
+      s->szB = chdr.ch_size;
+    } else if (h->sh_size > SIZE_OF_ZLIB_HEADER) {
+       /* Read the zlib header.  In this case, it should be "ZLIB"
+       followed by the uncompressed section size, 8 bytes in BE order. */
+       UChar tmp[SIZE_OF_ZLIB_HEADER];
+       ML_(img_get)(tmp, s->img, s->ioff, SIZE_OF_ZLIB_HEADER);
+       if (VG_(memcmp)(tmp, "ZLIB", 4) == 0) {
+          SizeT size;
+#         if (VG_WORDSIZE == 8)
+             size = tmp[4]; size <<= 8;
+             size += tmp[5]; size <<= 8;
+             size += tmp[6]; size <<= 8;
+             size += tmp[7]; size <<= 8;
+#         else
+             vg_assert((tmp[4] == 0) && (tmp[5] == 0) && (tmp[6] == 0)
+                       && (tmp[7] == 0));
+             size = 0;
+#         endif
+          size += tmp[8]; size <<= 8;
+          size += tmp[9]; size <<= 8;
+          size += tmp[10]; size <<= 8;
+          size += tmp[11];
+          s->ioff = ML_(img_mark_compressed_part)(s->img,
+                                                  s->ioff + SIZE_OF_ZLIB_HEADER,
+                                                  s->szB - SIZE_OF_ZLIB_HEADER,
+                                                  size);
+          s->szB = size;
+       }
+    }
+    return True;
+}
 
 /* The central function for reading ELF debug info.  For the
    object/exe specified by the DebugInfo, find ELF sections, then read
@@ -2407,15 +2480,20 @@ Bool ML_(read_elf_debug_info) ( struct _DebugInfo* di )
                _sec_escn.img  = mimg; \
                _sec_escn.ioff = (DiOffT)a_shdr.sh_offset; \
                _sec_escn.szB  = a_shdr.sh_size; \
+               if (!check_compression(&a_shdr, &_sec_escn)) { \
+                  ML_(symerr)(di, True, "   Compression type is unsupported"); \
+                  goto out; \
+               } \
                nobits         = a_shdr.sh_type == SHT_NOBITS; \
                vg_assert(_sec_escn.img  != NULL); \
                vg_assert(_sec_escn.ioff != DiOffT_INVALID); \
                TRACE_SYMTAB( "%-18s:  ioff %llu .. %llu\n", \
-                             _sec_name, (ULong)_sec_escn.ioff, \
-                             ((ULong)_sec_escn.ioff) + _sec_escn.szB - 1); \
+                             _sec_name, (ULong)a_shdr.sh_offset, \
+                             ((ULong)a_shdr.sh_offset) + a_shdr.sh_size - 1); \
                /* SHT_NOBITS sections have zero size in the file. */ \
                if (!nobits && \
-                   a_shdr.sh_offset + _sec_escn.szB > ML_(img_size)(mimg) ) { \
+                   a_shdr.sh_offset + \
+                      a_shdr.sh_size > ML_(img_real_size)(mimg)) { \
                   ML_(symerr)(di, True, \
                               "   section beyond image end?!"); \
                   goto out; \
@@ -2428,33 +2506,56 @@ Bool ML_(read_elf_debug_info) ( struct _DebugInfo* di )
 #        define FIND(_sec_name, _sec_escn) \
             FINDX(_sec_name, _sec_escn, /**/)
 
-         /*   NAME                  ElfSec */
-         FIND(".dynsym",            dynsym_escn)
-         FIND(".dynstr",            dynstr_escn)
-         FIND(".symtab",            symtab_escn)
-         FIND(".strtab",            strtab_escn)
+         /*      NAME                  ElfSec */
+         FIND(   ".dynsym",            dynsym_escn)
+         FIND(   ".dynstr",            dynstr_escn)
+         FIND(   ".symtab",            symtab_escn)
+         FIND(   ".strtab",            strtab_escn)
 #        if defined(VGO_solaris)
-         FIND(".SUNW_ldynsym",      ldynsym_escn)
+         FIND(   ".SUNW_ldynsym",      ldynsym_escn)
 #        endif
 
-         FIND(".gnu_debuglink",     debuglink_escn)
-         FIND(".gnu_debugaltlink",  debugaltlink_escn)
+         FIND(   ".gnu_debuglink",     debuglink_escn)
+         FIND(   ".gnu_debugaltlink",  debugaltlink_escn)
 
-         FIND(".debug_line",        debug_line_escn)
-         FIND(".debug_info",        debug_info_escn)
-         FIND(".debug_types",       debug_types_escn)
-         FIND(".debug_abbrev",      debug_abbv_escn)
-         FIND(".debug_str",         debug_str_escn)
-         FIND(".debug_ranges",      debug_ranges_escn)
-         FIND(".debug_loc",         debug_loc_escn)
-         FIND(".debug_frame",       debug_frame_escn)
+         FIND(   ".debug_line",        debug_line_escn)
+         if (!ML_(sli_is_valid)(debug_line_escn))
+            FIND(".zdebug_line",       debug_line_escn)
 
-         FIND(".debug",             dwarf1d_escn)
-         FIND(".line",              dwarf1l_escn)
+         FIND(   ".debug_info",        debug_info_escn)
+         if (!ML_(sli_is_valid)(debug_info_escn))
+            FIND(".zdebug_info",       debug_info_escn)
 
-         FIND(".opd",               opd_escn)
+         FIND(   ".debug_types",       debug_types_escn)
+         if (!ML_(sli_is_valid)(debug_types_escn))
+            FIND(".zdebug_types",      debug_types_escn)
 
-         FINDX(".eh_frame",         ehframe_escn[ehframe_mix],
+         FIND(   ".debug_abbrev",      debug_abbv_escn)
+         if (!ML_(sli_is_valid)(debug_abbv_escn))
+            FIND(".zdebug_abbrev",     debug_abbv_escn)
+
+         FIND(   ".debug_str",         debug_str_escn)
+         if (!ML_(sli_is_valid)(debug_str_escn))
+            FIND(".zdebug_str",        debug_str_escn)
+
+         FIND(   ".debug_ranges",      debug_ranges_escn)
+         if (!ML_(sli_is_valid)(debug_ranges_escn))
+            FIND(".zdebug_ranges",     debug_ranges_escn)
+
+         FIND(   ".debug_loc",         debug_loc_escn)
+         if (!ML_(sli_is_valid)(debug_loc_escn))
+            FIND(".zdebug_loc",    debug_loc_escn)
+
+         FIND(   ".debug_frame",       debug_frame_escn)
+         if (!ML_(sli_is_valid)(debug_frame_escn))
+            FIND(".zdebug_frame",      debug_frame_escn)
+
+         FIND(   ".debug",             dwarf1d_escn)
+         FIND(   ".line",              dwarf1l_escn)
+
+         FIND(   ".opd",               opd_escn)
+
+         FINDX(  ".eh_frame",          ehframe_escn[ehframe_mix],
                do { ehframe_mix++; vg_assert(ehframe_mix <= N_EHFRAME_SECTS);
                } while (0)
          )
@@ -2704,16 +2805,20 @@ Bool ML_(read_elf_debug_info) ( struct _DebugInfo* di )
                   _sec_escn.img  = dimg; \
                   _sec_escn.ioff = (DiOffT)a_shdr.sh_offset;  \
                   _sec_escn.szB  = a_shdr.sh_size; \
+                  if (!check_compression(&a_shdr, &_sec_escn)) { \
+                     ML_(symerr)(di, True, "   Compression type is unsupported"); \
+                     goto out; \
+                  } \
                   nobits         = a_shdr.sh_type == SHT_NOBITS; \
                   vg_assert(_sec_escn.img  != NULL); \
                   vg_assert(_sec_escn.ioff != DiOffT_INVALID); \
                   TRACE_SYMTAB( "%-18s: dioff %llu .. %llu\n", \
                                 _sec_name, \
-                                (ULong)_sec_escn.ioff, \
-                                ((ULong)_sec_escn.ioff) + _sec_escn.szB - 1); \
+                                (ULong)a_shdr.sh_offset, \
+                                ((ULong)a_shdr.sh_offset) + a_shdr.sh_size - 1); \
                   /* SHT_NOBITS sections have zero size in the file. */ \
                   if (!nobits && a_shdr.sh_offset \
-                      + _sec_escn.szB > ML_(img_size)(dimg)) { \
+                      + a_shdr.sh_size > ML_(img_real_size)(_sec_escn.img)) { \
                      ML_(symerr)(di, True, \
                                  "   section beyond image end?!"); \
                      goto out; \
@@ -2721,24 +2826,45 @@ Bool ML_(read_elf_debug_info) ( struct _DebugInfo* di )
                } \
             } while (0);
 
-            /* NEEDED?        NAME             ElfSec */
-            FIND(need_symtab, ".symtab",       symtab_escn)
-            FIND(need_symtab, ".strtab",       strtab_escn)
-            FIND(need_dwarf2, ".debug_line",   debug_line_escn)
-            FIND(need_dwarf2, ".debug_info",   debug_info_escn)
-            FIND(need_dwarf2, ".debug_types",  debug_types_escn)
+            /* NEEDED?               NAME                 ElfSec */
+            FIND(   need_symtab,     ".symtab",           symtab_escn)
+            FIND(   need_symtab,     ".strtab",           strtab_escn)
+            FIND(   need_dwarf2,     ".debug_line",       debug_line_escn)
+            if (!ML_(sli_is_valid)(debug_line_escn))
+               FIND(need_dwarf2,     ".zdebug_line",      debug_line_escn)
 
-            FIND(need_dwarf2, ".debug_abbrev", debug_abbv_escn)
-            FIND(need_dwarf2, ".debug_str",    debug_str_escn)
-            FIND(need_dwarf2, ".debug_ranges", debug_ranges_escn)
+            FIND(   need_dwarf2,     ".debug_info",       debug_info_escn)
+            if (!ML_(sli_is_valid)(debug_info_escn))
+               FIND(need_dwarf2,     ".zdebug_info",      debug_info_escn)
 
-            FIND(need_dwarf2, ".debug_loc",    debug_loc_escn)
-            FIND(need_dwarf2, ".debug_frame",  debug_frame_escn)
+            FIND(   need_dwarf2,     ".debug_types",      debug_types_escn)
+            if (!ML_(sli_is_valid)(debug_types_escn))
+               FIND(need_dwarf2,     ".zdebug_types",     debug_types_escn)
 
-            FIND(need_dwarf2, ".gnu_debugaltlink", debugaltlink_escn)
+            FIND(   need_dwarf2,     ".debug_abbrev",     debug_abbv_escn)
+            if (!ML_(sli_is_valid)(debug_abbv_escn))
+               FIND(need_dwarf2,     ".zdebug_abbrev",    debug_abbv_escn)
 
-            FIND(need_dwarf1, ".debug",        dwarf1d_escn)
-            FIND(need_dwarf1, ".line",         dwarf1l_escn)
+            FIND(   need_dwarf2,     ".debug_str",        debug_str_escn)
+            if (!ML_(sli_is_valid)(debug_str_escn))
+               FIND(need_dwarf2,     ".zdebug_str",       debug_str_escn)
+
+            FIND(   need_dwarf2,     ".debug_ranges",     debug_ranges_escn)
+            if (!ML_(sli_is_valid)(debug_ranges_escn))
+               FIND(need_dwarf2,     ".zdebug_ranges",    debug_ranges_escn)
+
+            FIND(   need_dwarf2,     ".debug_loc",        debug_loc_escn)
+            if (!ML_(sli_is_valid)(debug_loc_escn))
+               FIND(need_dwarf2,     ".zdebug_loc",       debug_loc_escn)
+
+            FIND(   need_dwarf2,     ".debug_frame",      debug_frame_escn)
+            if (!ML_(sli_is_valid)(debug_frame_escn))
+               FIND(need_dwarf2,     ".zdebug_frame",     debug_frame_escn)
+
+            FIND(   need_dwarf2,     ".gnu_debugaltlink", debugaltlink_escn)
+
+            FIND(   need_dwarf1,     ".debug",            dwarf1d_escn)
+            FIND(   need_dwarf1,     ".line",             dwarf1l_escn)
 
 #           undef FIND
          } /* Find all interesting sections */
@@ -2848,20 +2974,36 @@ Bool ML_(read_elf_debug_info) ( struct _DebugInfo* di )
                   _sec_escn.img  = aimg; \
                   _sec_escn.ioff = (DiOffT)a_shdr.sh_offset; \
                   _sec_escn.szB  = a_shdr.sh_size; \
+                  if (!check_compression(&a_shdr, &_sec_escn)) { \
+                     ML_(symerr)(di, True, "   Compression type is " \
+                                           "unsupported"); \
+                     goto out; \
+                  } \
                   vg_assert(_sec_escn.img  != NULL); \
                   vg_assert(_sec_escn.ioff != DiOffT_INVALID); \
                   TRACE_SYMTAB( "%-18s: aioff %llu .. %llu\n", \
                                 _sec_name, \
-                                (ULong)_sec_escn.ioff, \
-                                ((ULong)_sec_escn.ioff) + _sec_escn.szB - 1); \
+                                (ULong)a_shdr.sh_offset, \
+                                ((ULong)a_shdr.sh_offset) + a_shdr.sh_size - 1); \
                } \
             } while (0);
 
-            /*   NAME             ElfSec */
-            FIND(".debug_line",   debug_line_alt_escn)
-            FIND(".debug_info",   debug_info_alt_escn)
-            FIND(".debug_abbrev", debug_abbv_alt_escn)
-            FIND(".debug_str",    debug_str_alt_escn)
+            /*   NAME                 ElfSec */
+            FIND(".debug_line",       debug_line_alt_escn)
+            if (!ML_(sli_is_valid)(debug_line_alt_escn))
+               FIND(".zdebug_line",   debug_line_alt_escn)
+
+            FIND(".debug_info",       debug_info_alt_escn)
+            if (!ML_(sli_is_valid)(debug_info_alt_escn))
+               FIND(".zdebug_info",   debug_info_alt_escn)
+
+            FIND(".debug_abbrev",     debug_abbv_alt_escn)
+            if (!ML_(sli_is_valid)(debug_abbv_alt_escn))
+               FIND(".zdebug_abbrev", debug_abbv_alt_escn)
+
+            FIND(".debug_str",        debug_str_alt_escn)
+            if (!ML_(sli_is_valid)(debug_str_alt_escn))
+               FIND(".zdebug_str",    debug_str_alt_escn)
 
 #           undef FIND
          } /* Find all interesting sections */

configure.ac
@@ -1202,6 +1202,11 @@ AC_MSG_RESULT([no])
 ])
 
 
+# Check for ELF32/64_CHDR
+
+AC_CHECK_TYPES([Elf32_Chdr, Elf64_Chdr], [], [], [[#include <elf.h>]])
+
+
 # Check for PTHREAD_RWLOCK_T
 
 AC_MSG_CHECKING([for pthread_rwlock_t])
@@ -2089,6 +2094,46 @@ ac_have_dwarf4=no
 AC_MSG_RESULT([no])
 ])
 AM_CONDITIONAL(DWARF4, test x$ac_have_dwarf4 = xyes)
+CFLAGS=$safe_CFLAGS
+
+
+# does this compiler support -g -gz=zlib ?
+
+AC_MSG_CHECKING([if gcc accepts -g -gz=zlib])
+
+safe_CFLAGS=$CFLAGS
+CFLAGS="-g -gz=zlib"
+
+AC_COMPILE_IFELSE([AC_LANG_PROGRAM([[ ]], [[
+  return 0;
+]])], [
+ac_have_gz_zlib=yes
+AC_MSG_RESULT([yes])
+], [
+ac_have_gz_zlib=no
+AC_MSG_RESULT([no])
+])
+AM_CONDITIONAL(GZ_ZLIB, test x$ac_have_gz_zlib = xyes)
+CFLAGS=$safe_CFLAGS
+
+
+# does this compiler support -g -gz=zlib-gnu ?
+
+AC_MSG_CHECKING([if gcc accepts -g -gz=zlib-gnu])
+
+safe_CFLAGS=$CFLAGS
+CFLAGS="-g -gz=zlib-gnu"
+
+AC_COMPILE_IFELSE([AC_LANG_PROGRAM([[ ]], [[
+  return 0;
+]])], [
+ac_have_gz_zlib_gnu=yes
+AC_MSG_RESULT([yes])
+], [
+ac_have_gz_zlib_gnu=no
+AC_MSG_RESULT([no])
+])
+AM_CONDITIONAL(GZ_ZLIB_GNU, test x$ac_have_gz_zlib_gnu = xyes)
 CFLAGS=$safe_CFLAGS
 
 
@@ -3535,7 +3580,6 @@ if test "$solaris_fpchip_state_takes_underscore" = "yes"; then
   AC_DEFINE(SOLARIS_FPCHIP_STATE_TAKES_UNDERSCORE, 1,
             [Define to 1 if fpregset_t defines struct _fpchip_state])
 fi
-
 
 else
 AM_CONDITIONAL(SOLARIS_SUN_STUDIO_AS, false)

coregrind/Makefile.am
@@ -351,6 +351,7 @@ COREGRIND_SOURCES_COMMON = \
 	m_debuginfo/readmacho.c \
 	m_debuginfo/readpdb.c \
 	m_debuginfo/storage.c \
+	m_debuginfo/tinfl.c \
 	m_debuginfo/tytypes.c \
 	m_demangle/cp-demangle.c \
 	m_demangle/cplus-dem.c \

coregrind/m_debuginfo/image.c
@@ -45,6 +45,8 @@
 #include "priv_image.h"            /* self */
 
 #include "minilzo.h"
+#define TINFL_HEADER_FILE_ONLY
+#include "tinfl.c"
 
 /* These values (1024 entries of 8192 bytes each) gives a cache
    size of 8MB. */
@@ -53,14 +55,27 @@
 
 #define CACHE_ENTRY_SIZE      (1 << CACHE_ENTRY_SIZE_BITS)
 
+#define COMMPRESSED_SLICE_ARRAY_GROW_SIZE 64
+
 /* An entry in the cache. */
 typedef
    struct {
-      DiOffT off; // file offset for data[0]
-      SizeT  used; // 1 .. sizeof(data), or 0 to denote not-in-use
-      UChar  data[CACHE_ENTRY_SIZE];
+      DiOffT off;    // file offset for data[0]
+      SizeT  size;   // sizeof(data)
+      SizeT  used;   // 1 .. sizeof(data), or 0 to denote not-in-use
+      UChar  data[];
    }
    CEnt;
+
+/* Compressed slice */
+typedef
+   struct {
+      DiOffT offD;  // offset of decompressed data
+      SizeT  szD;   // size of decompressed data
+      DiOffT offC;  // offset of compressed data
+      SizeT  szC;   // size of compressed data
+   }
+   CSlc;
 
 /* Source for files */
 typedef
@@ -82,8 +97,10 @@ typedef
 struct _DiImage {
    // The source -- how to get hold of the file we are reading
    Source source;
-   // Total size of the image.
+   // Virtual size of the image = real size + size of uncompressed data
    SizeT size;
+   // Real size of image
+   SizeT real_size;
    // The number of entries used.  0 .. CACHE_N_ENTRIES
    UInt  ces_used;
    // Pointers to the entries.  ces[0 .. ces_used-1] are non-NULL.
@@ -91,6 +108,13 @@ struct _DiImage {
    // The non-NULL entries may be arranged arbitrarily.  We expect to use
    // a pseudo-LRU scheme though.
    CEnt* ces[CACHE_N_ENTRIES];
+
+   // Array of compressed slices
+   CSlc* cslc;
+   // Number of compressed slices used
+   UInt  cslc_used;
+   // Size of cslc array
+   UInt  cslc_size;
 };
 
 /* A frame.  The first 4 bytes of |data| give the kind of the frame,
@@ -395,7 +419,7 @@ static inline Bool is_in_CEnt ( const CEnt* cent, DiOffT off )
    /* This assertion is checked by set_CEnt, so checking it here has
       no benefit, whereas skipping it does remove it from the hottest
       path. */
-   /* vg_assert(cent->used > 0 && cent->used <= CACHE_ENTRY_SIZE); */
+   /* vg_assert(cent->used > 0 && cent->used <= cent->size); */
    /* What we want to return is:
         cent->off <= off && off < cent->off + cent->used;
       This is however a very hot path, so here's alternative that uses
@@ -415,16 +439,39 @@ static inline Bool is_in_CEnt ( const CEnt* cent, DiOffT off )
    return off - cent->off < cent->used;
 }
 
-/* Allocate a new CEnt, connect it to |img|, and return its index. */
-static UInt alloc_CEnt ( DiImage* img )
+/* Returns pointer to CSlc or NULL */
+static inline CSlc* find_cslc ( DiImage* img, DiOffT off )
 {
-   vg_assert(img);
+   for (UInt i = 0; i < img->cslc_used; i++) {
+      if ( (img->cslc[i].offD <= off)
+           && (img->cslc[i].offD + img->cslc[i].szD > off)
+         )
+         return &img->cslc[i];
+   }
+   return NULL;
+}
+
+/* Allocate a new CEnt, connect it to |img|, and return its index. */
+static UInt alloc_CEnt ( DiImage* img, SizeT szB )
+{
+   vg_assert(img != NULL);
    vg_assert(img->ces_used < CACHE_N_ENTRIES);
+   vg_assert(szB >= CACHE_ENTRY_SIZE);
    UInt entNo = img->ces_used;
    img->ces_used++;
    vg_assert(img->ces[entNo] == NULL);
-   img->ces[entNo] = ML_(dinfo_zalloc)("di.alloc_CEnt.1", sizeof(CEnt));
+   img->ces[entNo] = ML_(dinfo_zalloc)("di.alloc_CEnt.1",
+                                       offsetof(CEnt, data) + szB);
+   img->ces[entNo]->size = szB;
    return entNo;
+}
+
+static void realloc_CEnt ( DiImage* img, UInt entNo, SizeT szB ) {
+   vg_assert(img != NULL);
+   vg_assert(szB >= CACHE_ENTRY_SIZE);
+   img->ces[entNo] = ML_(dinfo_realloc)("di.realloc_CEnt.1",
+                                        img->ces[entNo],
+                                        offsetof(CEnt, data) + szB);
 }
 
 /* Move the given entry to the top and slide those above it down by 1,
@@ -449,23 +496,24 @@ static void set_CEnt ( const DiImage* img, UInt entNo, DiOffT off )
 {
    SizeT len;
    DiOffT off_orig = off;
-   vg_assert(img);
+   vg_assert(img != NULL);
    vg_assert(img->ces_used <= CACHE_N_ENTRIES);
    vg_assert(entNo >= 0 && entNo < img->ces_used);
-   vg_assert(off < img->size);
-   vg_assert(img->ces[entNo] != NULL);
+   vg_assert(off < img->real_size);
+   CEnt* ce = img->ces[entNo];
+   vg_assert(ce != NULL);
    /* Compute [off, +len) as the slice we are going to read. */
    off = block_round_down(off);
-   len = img->size - off;
-   if (len > CACHE_ENTRY_SIZE) len = CACHE_ENTRY_SIZE;
+   len = img->real_size - off;
+   if (len > ce->size)
+      len = ce->size;
    /* It is conceivable that the 'len > 0' bit could fail if we make
       an image with a zero sized file.  But then no 'get' request on
       that image would be valid. */
-   vg_assert(len > 0 && len <= CACHE_ENTRY_SIZE);
-   vg_assert(off + len <= img->size);
+   vg_assert(len > 0 && len <= ce->size);
+   vg_assert(off + len <= img->real_size);
    vg_assert(off <= off_orig && off_orig < off+len);
    /* So, read  off .. off+len-1  into the entry. */
-   CEnt* ce = img->ces[entNo];
 
    if (0) {
       static UInt t_last = 0;
@@ -546,7 +594,7 @@ static void set_CEnt ( const DiImage* img, UInt entNo, DiOffT off )
    
    ce->off  = off;
    ce->used = len;
-   vg_assert(ce->used > 0 && ce->used <= CACHE_ENTRY_SIZE);
+   vg_assert(ce->used > 0 && ce->used <= ce->size);
 }
 
 __attribute__((noinline))
@@ -567,16 +615,45 @@ static UChar get_slowcase ( DiImage* img, DiOffT off )
    if (i == img->ces_used) {
       /* It's not in any entry.  Either allocate a new entry or
          recycle the LRU one. */
+
+      CSlc* cslc = find_cslc(img, off);
+      UChar* buf = NULL;
+      if (cslc != NULL) {
+         SizeT len = 0;
+         buf = ML_(dinfo_zalloc)("di.image.get_slowcase.1", cslc->szC);
+         // get compressed data
+         while (len < cslc->szC)
+            len += ML_(img_get_some)(buf + len, img, cslc->offC + len,
+                                     cslc->szC - len);
+      }
+
       if (img->ces_used == CACHE_N_ENTRIES) {
          /* All entries in use.  Recycle the (ostensibly) LRU one. */
-         set_CEnt(img, CACHE_N_ENTRIES-1, off);
          i = CACHE_N_ENTRIES-1;
+         if ((cslc != NULL) && (cslc->szD > img->ces[i]->size))
+            realloc_CEnt(img, i, cslc->szD);
       } else {
          /* Allocate a new one, and fill it in. */
-         UInt entNo = alloc_CEnt(img);
-         set_CEnt(img, entNo, off);
-         i = entNo;
+         SizeT size = CACHE_ENTRY_SIZE;
+         if ((cslc != NULL) && (cslc->szD > CACHE_ENTRY_SIZE))
+            size = cslc->szD;
+         i = alloc_CEnt(img, size);
       }
+
+      if (cslc != NULL) {
+         SizeT len = tinfl_decompress_mem_to_mem(
+                        img->ces[i]->data, cslc->szD,
+                        buf, cslc->szC,
+                        TINFL_FLAG_USING_NON_WRAPPING_OUTPUT_BUF
+                        | TINFL_FLAG_PARSE_ZLIB_HEADER);
+         vg_assert(len == cslc->szD);
+         img->ces[i]->used = cslc->szD;
+         img->ces[i]->off = cslc->offD;
+         ML_(dinfo_free)(buf);
+      } else {
+         set_CEnt(img, i, off);
+      }
+
    } else {
       /* We found it at position 'i'. */
       vg_assert(i > 0);
@@ -633,8 +710,12 @@ DiImage* ML_(img_from_local_file)(const HChar* fullpath)
    img->source.is_local = True;
    img->source.fd       = sr_Res(fd);
    img->size            = size;
+   img->real_size       = size;
    img->ces_used        = 0;
    img->source.name     = ML_(dinfo_strdup)("di.image.ML_iflf.2", fullpath);
+   img->cslc            = NULL;
+   img->cslc_size       = 0;
+   img->cslc_used       = 0;
    /* img->ces is already zeroed out */
    vg_assert(img->source.fd >= 0);
 
@@ -643,7 +724,7 @@ DiImage* ML_(img_from_local_file)(const HChar* fullpath)
       loading it at this point forcing img->cent[0] to always be
       non-empty, thereby saving us an is-it-empty check on the fast
       path in get(). */
-   UInt entNo = alloc_CEnt(img);
+   UInt entNo = alloc_CEnt(img, CACHE_ENTRY_SIZE);
    vg_assert(entNo == 0);
    set_CEnt(img, 0, 0);
 
@@ -719,18 +800,22 @@ DiImage* ML_(img_from_di_server)(const HChar* filename,
    img->source.fd         = sd;
    img->source.session_id = session_id;
    img->size              = size;
+   img->real_size         = size;
    img->ces_used          = 0;
    img->source.name       = ML_(dinfo_zalloc)("di.image.ML_ifds.2",
                                               20 + VG_(strlen)(filename)
                                                  + VG_(strlen)(serverAddr));
    VG_(sprintf)(img->source.name, "%s at %s", filename, serverAddr);
+   img->cslc            = NULL;
+   img->cslc_size       = 0;
+   img->cslc_used       = 0;
 
    /* img->ces is already zeroed out */
    vg_assert(img->source.fd >= 0);
 
    /* See comment on equivalent bit in ML_(img_from_local_file) for
       rationale. */
-   UInt entNo = alloc_CEnt(img);
+   UInt entNo = alloc_CEnt(img, CACHE_ENTRY_SIZE);
    vg_assert(entNo == 0);
    set_CEnt(img, 0, 0);
 
@@ -756,9 +841,32 @@ DiImage* ML_(img_from_di_server)(const HChar* filename,
    return NULL;
 }
 
+DiOffT ML_(img_mark_compressed_part)(DiImage* img, DiOffT offset, SizeT szC,
+                                     SizeT szD)
+{
+   DiOffT ret;
+   vg_assert(img != NULL);
+   vg_assert(offset + szC <= img->size);
+
+   if (img->cslc_used == img->cslc_size) {
+      img->cslc_size += COMMPRESSED_SLICE_ARRAY_GROW_SIZE;
+      img->cslc = ML_(dinfo_realloc)("di.image.ML_img_mark_compressed_part.1",
+                                     img->cslc, img->cslc_size * sizeof(CSlc));
+   }
+
+   ret = img->size;
+   img->cslc[img->cslc_used].offC = offset;
+   img->cslc[img->cslc_used].szC = szC;
+   img->cslc[img->cslc_used].offD = img->size;
+   img->cslc[img->cslc_used].szD = szD;
+   img->size += szD;
+   img->cslc_used++;
+   return ret;
+}
+
 void ML_(img_done)(DiImage* img)
 {
-   vg_assert(img);
+   vg_assert(img != NULL);
    if (img->source.is_local) {
       /* Close the file; nothing else to do. */
       vg_assert(img->source.session_id == 0);
@@ -782,18 +890,25 @@ void ML_(img_done)(DiImage* img)
       vg_assert(img->ces[i] == NULL);
    }
    ML_(dinfo_free)(img->source.name);
+   ML_(dinfo_free)(img->cslc);
    ML_(dinfo_free)(img);
 }
 
 DiOffT ML_(img_size)(const DiImage* img)
 {
-   vg_assert(img);
+   vg_assert(img != NULL);
    return img->size;
+}
+
+DiOffT ML_(img_real_size)(const DiImage* img)
+{
+   vg_assert(img != NULL);
+   return img->real_size;
 }
 
 inline Bool ML_(img_valid)(const DiImage* img, DiOffT offset, SizeT size)
 {
-   vg_assert(img);
+   vg_assert(img != NULL);
    vg_assert(offset != DiOffT_INVALID);
    return img->size > 0 && offset + size <= (DiOffT)img->size;
 }
@@ -830,7 +945,7 @@ static void ensure_valid(const DiImage* img, DiOffT offset, SizeT size,
 void ML_(img_get)(/*OUT*/void* dst,
                   DiImage* img, DiOffT offset, SizeT size)
 {
-   vg_assert(img);
+   vg_assert(img != NULL);
    vg_assert(size > 0);
    ensure_valid(img, offset, size, "ML_(img_get)");
    SizeT i;
@@ -842,7 +957,7 @@ void ML_(img_get)(/*OUT*/void* dst,
 SizeT ML_(img_get_some)(/*OUT*/void* dst,
                         DiImage* img, DiOffT offset, SizeT size)
 {
-   vg_assert(img);
+   vg_assert(img != NULL);
    vg_assert(size > 0);
    ensure_valid(img, offset, size, "ML_(img_get_some)");
    UChar* dstU = (UChar*)dst;
@@ -1002,7 +1117,7 @@ UInt ML_(img_calc_gnu_debuglink_crc32)(DiImage* img)
       0x2d02ef8d
     };
 
-   vg_assert(img);
+   vg_assert(img != NULL);
 
    /* If the image is local, calculate the CRC here directly.  If it's
       remote, forward the request to the server. */

commit 0fcaf4288b2ced4d717d93e055ace6e5b85d69e7

Fix compilation breakage on platforms that do already have a definition for Elf{32,64}_Chdr.  This is fallout from r15868.  That commit provided a configure test, but the resulting config.h was not included here, causing the test results to be ignored.

coregrind/m_debuginfo/readelf.c
@@ -51,6 +51,7 @@
 #include "priv_readdwarf.h"        /* 'cos ELF contains DWARF */
 #include "priv_readdwarf3.h"
 #include "priv_readexidx.h"
+#include "config.h"
 
 /* --- !!! --- EXTERNAL HEADERS start --- !!! --- */
 #include <elf.h>